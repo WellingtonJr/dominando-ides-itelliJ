69da880a9765f8f465f3cc3fbfc7c9fab27af494
WellingtonJr <wferreira.bm13@gmail.com>
2022-05-12 21:42:43 -0300

    criando class livro

--- src/br/com/dio/PrimeiroProgramaIntelliJ.java
+++ src/br/com/dio/PrimeiroProgramaIntelliJ.java
@@ -6,5 +6,45 @@ public class PrimeiroProgramaIntelliJ {
         int b = 3;
 
         System.out.println("ererer " + (a + b));
+
+        Livro livro = new Livro("Teste Nome Livro", 300);
+
+        System.out.println(livro);
+
+    }
+}
+
+
+class Livro {
+    private String nome;
+    private Integer npag;
+
+    public Livro(String nome, Integer npag) {
+        this.nome = nome;
+        this.npag = npag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNpag() {
+        return npag;
+    }
+
+    public void setNpag(Integer npag) {
+        this.npag = npag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", npag=" + npag +
+                '}';
     }
 }
